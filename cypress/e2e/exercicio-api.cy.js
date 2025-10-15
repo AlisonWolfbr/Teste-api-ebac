bf516f42e010fab15498631cc763c6b26d4ae8fe
AlisonWolfbr <alison.luan.gomes@hotmail.com>
2025-10-15 15:17:11 -0300

    Adiciona testes de API de usuários e produtos

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -3,28 +3,111 @@
 describe('Testes da Funcionalidade Usuários', () => {
 
   it('Deve validar contrato de usuários', () => {
-    //TODO: 
-  });
+    cy.request('usuarios').then((response) => {
+      expect(response.status).to.equal(200)
+      expect(response.body).to.have.property('usuarios')
+      expect(response.body.usuarios[0]).to.have.all.keys('_id', 'nome', 'email', 'password', 'administrador')
+    })
+  })
 
   it('Deve listar usuários cadastrados', () => {
-    //TODO: 
-  });
+    cy.request({
+      method: 'GET',
+      url: '/usuarios'
+    }).then((response) => {
+      expect(response.status).to.equal(200)
+      expect(response.body.usuarios).to.be.an('array')
+    })
+  })
 
   it('Deve cadastrar um usuário com sucesso', () => {
-    //TODO: 
-  });
+    const email = `usuario${Math.floor(Math.random() * 100000)}@qa.com`
+
+    cy.request({
+      method: 'POST',
+      url: '/usuarios',
+      body: {
+        nome: 'Usuário EBAC QA',
+        email: email,
+        password: 'teste',
+        administrador: 'true'
+      }
+    }).should((response) => {
+      expect(response.status).to.equal(201)
+      expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+    })
+  })
 
   it('Deve validar um usuário com email inválido', () => {
-    //TODO: 
-  });
+    cy.request({
+      method: 'POST',
+      url: '/usuarios',
+      failOnStatusCode: false,
+      body: {
+        nome: 'Usuário inválido',
+        email: 'invalido.com', 
+        password: 'teste',
+        administrador: 'true'
+      }
+    }).should((response) => {
+      expect(response.status).to.equal(400)
+    })
+  })
 
   it('Deve editar um usuário previamente cadastrado', () => {
-    //TODO: 
-  });
+    const email = `editar${Math.floor(Math.random() * 10000)}@qa.com`
+
+    cy.request({
+      method: 'POST',
+      url: '/usuarios',
+      body: {
+        nome: 'Usuário a Editar',
+        email: email,
+        password: '1234',
+        administrador: 'true'
+      }
+    }).then((response) => {
+      const id = response.body._id
+
+      cy.request({
+        method: 'PUT',
+        url: `/usuarios/${id}`,
+        body: {
+          nome: 'Usuário Editado',
+          email: email,
+          password: '4321',
+          administrador: 'false'
+        }
+      }).should((response) => {
+        expect(response.status).to.equal(200)
+        expect(response.body.message).to.equal('Registro alterado com sucesso')
+      })
+    })
+  })
 
   it('Deve deletar um usuário previamente cadastrado', () => {
-    //TODO: 
-  });
+    const email = `deletar${Math.floor(Math.random() * 10000)}@qa.com`
 
+    cy.request({
+      method: 'POST',
+      url: '/usuarios',
+      body: {
+        nome: 'Usuário para deletar',
+        email: email,
+        password: '1234',
+        administrador: 'true'
+      }
+    }).then((response) => {
+      const id = response.body._id
 
-});
+      cy.request({
+        method: 'DELETE',
+        url: `/usuarios/${id}`
+      }).should((response) => {
+        expect(response.status).to.equal(200)
+        expect(response.body.message).to.equal('Registro excluído com sucesso')
+      })
+    })
+  })
+})
+
